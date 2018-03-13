29e6bd283f806b483b9c3bb91b33e96e6f54afd2
Student <Student@193.107.34.219>
2018-03-13 11:58:53 +0100

    1

---
+++ Cities/src/Cities.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Cities {
+	private Map<String, List<String>> provinces = new HashMap<>();
+
+	public Cities() {
+		List<String> list1 = new ArrayList<>();
+		list1.add("Malbork");
+		list1.add("Gdansk");
+		list1.add("Tczew");
+		provinces.put("Pomorskie", list1);
+
+		List<String> list2 = new ArrayList<>();
+		list2.add("Elblag");
+		list2.add("Olsztyn");
+		list2.add("Wegorzewo");
+		list2.add("Paslek");
+		list2.add("Pisz");
+		provinces.put("WarminskoMazurskie", list2);
+	}
+
+}
